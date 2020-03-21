dd234f3ce96737f4d44a7192a9689d8260e2e4c4
Chuka Ekwenze <chuka.ue@gmail.com>
2020-03-21 23:08:50 +0100

    updated env variable name

--- src/_data/news.js
+++ src/_data/news.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 module.exports = async function(){
     try {
-        const response = await axios.get(`http://newsapi.org/v2/top-headlines?apiKey=${process.env.API_KEY}&country=us`);
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&country=us`);
         console.log(response);
 
         return response.data;
