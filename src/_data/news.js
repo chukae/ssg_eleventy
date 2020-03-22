947fc09ab284410b817782d8548b957ff3f6663d
Chuka Ekwenze <chuka.ue@gmail.com>
2020-03-22 10:57:36 +0100

    localize & redirects

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,16 +1,38 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 
 require("dotenv").config();
 
-
-module.exports = async function(){
+async function getNews(country){
     try {
-        const response = await axios.get(`http://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&country=us`);
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&country=${country}`);
         console.log(response);
 
-        return response.data;
+        return {
+            "country":country,
+            "articles":response.data.articles
+        }
         
     } catch (error) {
         console.log(error);
     }
 }
+
+
+module.exports = async function(){
+    var newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then(newsObjects=>{
+        console.log('newsObjects:', newsObjects);
+        return [].concat.apply([],newsObjects);
+    });
+    
+    // try {
+    //     const response = await axios.get(`http://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&country=us`);
+    //     console.log(response);
+
+    //     return response.data;
+        
+    // } catch (error) {
+    //     console.log(error);
+    // }
+};
